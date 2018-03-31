0d74fa59e7fcd970a5f8f5948d99c89eefd27a89
ImprobabilityCast <segmentationfault3@outlook.com>
2018-03-31 01:26:29 -0400

    added meta-pages

---
+++ wwwroot/meta-pages/template.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+<?php require $_SERVER['DOCUMENT_ROOT'] . "/meta-pages/includes.php"; ?>
+
+
+<body>
+
+
+
+
+</body>
+</html>

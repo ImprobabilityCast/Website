70de8985a934e21893557863ce741975ac9753ad
ImprobabilityCast <segmentationfault3@outlook.com>
2018-03-31 01:20:17 -0400

    added fortune to master

---
+++ wwwroot/fortune/updateFortune.php
@@ -0,0 +1,11 @@
+<?php
+
+if ($_GET['cat'] != "") {
+	$arg = escapeshellarg(stripslashes($_GET['cat']));
+} else {
+	$arg = "";
+}
+
+echo htmlspecialchars(shell_exec("/usr/games/fortune " . $arg));
+
+?>

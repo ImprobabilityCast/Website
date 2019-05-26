dd8924942f50c5daa04e0c5231f15f5c1f739c48
ImprobabilityCast <segmentationfault3@outlook.com>
2019-05-26 03:19:05 -0400

    mechanics + scoring visuals

--- wwwroot/pong/pong.js
+++ wwwroot/pong/pong.js
@@ -4,44 +4,78 @@
     canvas.height = window.innerHeight;
 
     var ctx = canvas.getContext("2d");
-    ctx.font = "100px PressStart2P";
 
-    const INITIAL_VX = -120; // px per sec
-    const INITIAL_VY = 120; // +y is down
+    var ScoreFontSize = window.innerWidth;
+    if (ScoreFontSize > 600) {
+        ScoreFontSize = 600;
+    }
+    ctx.font = ScoreFontSize + "% PressStart2P";
+
+    const INITIAL_VX = -180; // px per sec
+    const INITIAL_VY = 180; // +y is down
     const PADDLE_HEIGHT = 40;
+    const BALL_RADIUS = 5;
+    const MID_SCREEN_X = window.innerWidth / 2;
 
     var ball = newBall();
 
     var pLeft = {
-        x1: 60,
-        x2: 68,
+        deltaY: 0,
+        deltaT: 0,
+        vel: 0,
+        x1: window.innerWidth / 15,
+        x2: window.innerWidth / 15 + 8,
         y1: window.innerHeight / 2,
         y2: window.innerHeight / 2 + PADDLE_HEIGHT
     };
 
-    var p1Score = 0;
-    var p2Score = 0;
+    var p1Score = 100;
+    var p2Score = 98;
     
     function newBall() {
         return {
-            x: window.innerWidth / 2, // spawn ball in middle of top of window
+            x: MID_SCREEN_X, // spawn ball in middle of top of window
             y: 0,
             vx: INITIAL_VX,
             vy: INITIAL_VY
         };
     }
 
-    function inYBox(hitbox, y) {
-        return y > hitbox.y1 && y < hitbox.y2;
+    function inBackHitbox(hitbox, ball) {
+        var lowerDist = (hitbox.y2 - ball.y) * (hitbox.y2 - ball.y)
+            + (hitbox.x1 - ball.x) * (hitbox.x1 - ball.x);
+        var upperDist = (hitbox.y1 - ball.y) * (hitbox.y1 - ball.y)
+            + (hitbox.x1 - ball.x) * (hitbox.x1 - ball.x);
+        return lowerDist < BALL_RADIUS * BALL_RADIUS
+            || upperDist < BALL_RADIUS * BALL_RADIUS;
+    }
+
+    function inFrontHitbox(hitbox, ball) {
+        var lowerDist = (hitbox.y2 - ball.y) * (hitbox.y2 - ball.y)
+            + (hitbox.x2 - ball.x) * (hitbox.x2 - ball.x);
+        var upperDist = (hitbox.y1 - ball.y) * (hitbox.y1 - ball.y)
+            + (hitbox.x2 - ball.x) * (hitbox.x2 - ball.x);
+        return lowerDist < BALL_RADIUS * BALL_RADIUS
+            || upperDist < BALL_RADIUS * BALL_RADIUS;
     }
 
     function checkHitboxes() {
-        if (inYBox(pLeft, ball.y) && ball.x < pLeft.x2) {
-            ball.x += 2 * (pLeft.x2 - ball.x);
+        // idk if this will work over velocity 480 px per sec b/c of how hit box is checked
+        if (inFrontHitbox(pLeft, ball)) {
+            //ball.x += 2 * (pLeft.x2 - ball.x);
             ball.vx = -ball.vx;
+            ball.vy += pLeft.vel;
+        }
+        if (inBackHitbox(pLeft, ball)) {
+            if (pLeft.vel == 0) {
+                ball.vy *= -1;
+            } else if (pLeft.vel < 0 == ball.vy < 0) {
+                ball.vy += pLeft.vel;
+            } else {
+                ball.vy = (ball.vy - pLeft.vel) * -1;
+            }
         }
     }
-
 
     function handleHitWall() {
 
@@ -75,25 +109,41 @@
         }
     }
 
+    function drawScore() {
+        ctx.beginPath();
+        ctx.fillStyle = "#79e";
+        if (Math.log10(p1Score) == draw.p1Spaces
+                && (draw.p1Spaces + 1) * draw.shift < MID_SCREEN_X) {
+            draw.p1Spaces += 1;
+        }
+        var p1 = MID_SCREEN_X - (draw.shift * draw.p1Spaces + window.innerWidth / 15);
+        ctx.fillText(p1Score, p1, window.innerHeight / 6,
+                MID_SCREEN_X - window.innerWidth / 15);
+
+        if (Math.log10(p2Score) == draw.p2Spaces
+                && (draw.p2Spaces + 1) * draw.shift < MID_SCREEN_X) {
+            draw.p2Spaces += 1;
+        }
+        var p2 = window.innerWidth - (draw.shift * draw.p2Spaces + window.innerWidth / 15);
+        ctx.fillText(p2Score, p2, window.innerHeight / 6,
+                MID_SCREEN_X - window.innerWidth / 15);
+    }
+
     function draw() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.beginPath();
         ctx.fillStyle = "#282828";
         ctx.rect(0, 0, canvas.width, canvas.height);
-        ctx.fill()
+        ctx.fill();
 
-        // draw score
-        ctx.beginPath();
-        ctx.fillStyle = "#79e";
-        var shift = (p1Score >= 100) ? 25 : 100;
-        ctx.fillText(p1Score, shift, 175);
-        var shift = (p2Score >= 100) ? 25 : 100;
-        ctx.fillText(p2Score, canvas.width / 2 + shift, 175);
+        drawScore();
+
+        // draw dividing line
 
         // draw ball
         ctx.beginPath();
         ctx.fillStyle = "#eee";
-        ctx.arc(ball.x, ball.y, 5, 0, 360);
+        ctx.arc(ball.x, ball.y, BALL_RADIUS, 0, 360);
         ctx.fill();
 
         // draw player 1
@@ -102,6 +152,14 @@
         ctx.fill();
 
         // draw player 2
+    }
+
+    draw.p1Spaces = 3;
+    draw.p2Spaces = 2;
+    {
+        let fStr = window.getComputedStyle(document.documentElement).fontSize;
+        fStr = fStr.substr(0, fStr.length - 2);
+        draw.shift = parseInt(fStr) * ScoreFontSize / 100;
     }
 
     function updatePosition() {
@@ -116,6 +174,7 @@
 
         draw();
         updatePosition.lastTime = time;
+        console.log(ball.vy);
     }
 
     updatePosition.lastTime = new Date().getTime();
@@ -141,10 +200,17 @@
     // window.onkeyup = function (e) {
 
     // };
-
-    window.onwheel = function (e) {
+    
+    function pLeftMove(e) {
         pLeft.y1 += e.deltaY * 10;
         pLeft.y2 += e.deltaY * 10;
+
+        pLeft.deltaY += e.deltaY * 10;
+        
+        var time = new Date().getTime();
+        pLeft.deltaT += time - pLeftMove.lastTime;
+        pLeftMove.lastTime = time;
+        
         if (pLeft.y1 < 0) {
             pLeft.y1 = 0;
             pLeft.y2 = PADDLE_HEIGHT;
@@ -155,6 +221,20 @@
         }
     };
 
+    pLeftMove.lastTime = new Date().getTime();
 
+    function updatePVel() {
+        if (pLeft.deltaT > 0) {
+            pLeft.vel = (pLeft.deltaY / pLeft.deltaT) * 3000;
+        }
+        if ((new Date().getTime() - pLeftMove.lastTime) > 400) {
+            pLeft.vel = 0;
+            pLeft.deltaY = 0;
+            pLeft.deltaT = 0;
+        }
+    }
+    
+    window.onwheel = pLeftMove;
+    setInterval(updatePVel, 300);
     var thing = setInterval(updatePosition, 16.6667, ball); // the # of milliseconds in 1s / 60
 })();

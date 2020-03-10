083cde9f698e4dd4ec1bcac15a9f5ce2eb7063fb
ImprobabilityCast <segmentationfault3@outlook.com>
2020-03-10 01:59:31 -0400

    day form proto done

--- wwwroot/mood/formTricks.js
+++ wwwroot/mood/formTricks.js
@@ -16,3 +16,39 @@ function radioBtnClick(e) {
 		t.classList.remove("btn-outline-primary");
 	}
 }
+
+
+function addMech(e) {
+	var form = $("#newMechName");
+	var name = form.val().trim();
+	if (name.length == 0) {
+		form.addClass("is-invalid");
+		form.siblings(".invalid-feedback")[0].style.display = "block";
+		return;
+	} else {
+		form.removeClass("is-invalid");
+		form.siblings(".invalid-feedback")[0].style.display = "none";
+	}
+	
+	var class_name = name.replace(/[^A-Z0-9]/ig, '-');
+	var cope = document.getElementById("coping");
+	var ele = document.createElement("div");
+	ele.classList.add("input-group");
+	ele.classList.add("form-row");
+	ele.innerHTML = '<label class="col-sm-4 col-form-label" for="'
+	+ class_name + '">'
+	+ name + ':</label>'
+	+ '<div class="col">'
+	+   '<div id="' + class_name + '" class="btn-group">'
+	+	  '<button type="button" class="btn btn-outline-primary" onclick="radioBtnClick(event)">It&nbsp;helped</button>'
+	+	  '<button type="button" class="btn btn-outline-primary" onclick="radioBtnClick(event)">Did&nbsp;not&nbsp;help</button>'
+	+	  '<button type="button" class="btn btn-outline-primary" onclick="radioBtnClick(event)">Not&nbsp;used</button>'
+	+   '</div>'
+	+ '</div>';
+	cope.appendChild(ele);
+}
+
+function removeMech(e) {
+	var par = e.currentTarget.parentElement;
+	par.parentElement.removeChild(par);
+}

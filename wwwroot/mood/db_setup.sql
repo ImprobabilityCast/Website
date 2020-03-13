897c473a05fac7905becc23314d4943d60052a22
ImprobabilityCast <segmentationfault3@outlook.com>
2020-03-13 01:13:47 -0400

    basic key drivation

--- wwwroot/mood/db_setup.sql
+++ wwwroot/mood/db_setup.sql
@@ -10,11 +10,23 @@ CREATE TABLE Users (
 	pwd_dkey char(128),
 	pwd_hash char(60),
 	email_hash char(60),
-	email_mask varchar(254),
 	PRIMARY KEY(id)
 );
 
-INSERT INTO Users (email_mask)
-	VALUES("a****@sugarfairyland.com");
+CREATE PROCEDURE mood.add_user
+		(IN recovery_dkey, IN pwd_dkey, IN pwd_hash, IN email_hash)
+BEGIN
+	INSERT INTO mood.Users (recovery_dkey, pwd_dkey, pwd_hash, email_hash)
+		VALUES(recovery_dkey, pwd_dkey, pwd_hash, email_hash);
+END
+
+-- setup demo account
+mood.add_user(
+	-- recovery_dkey
+	-- pwd_dkey
+	-- demo@sugarfairyland.com hash
+	"$2y$10$HqboaQRbrSxcJKt9n.gwau1dWfIr7rv53tCPAF.9zGO738JhTASia"
+	-- hunter2 hash
+	"$2y$10$Q7DKrvLsSgjDlqMBIcI9YeYfA7UKqmqhfBkdoO31j4APB6MNBMiXG");
 
 SELECT * FROM Users;
--- wwwroot/mood/signup_action.php
+++ wwwroot/mood/signup_action.php
@@ -1,7 +1,48 @@
 <?php
+
+function badInput() {
+	
+}
+
+
 // validation
 
-// call sql function
+// email regex
+// https://stackoverflow.com/a/8829363/8335309
+// https://html.spec.whatwg.org/multipage/input.html#valid-e-mail-address
+$email_regex = '/\A[a-zA-Z0-9.!#$%&\'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*\z/';
+
+if (FALSE === preg_match($email_regex, $_POST['email'])) {
+	
+}
+
+// check password requirements
+if (strlen($_POST['pass1']) < 16
+		|| FALSE === preg_match('/[a-z]+/i', $_POST['pass1'])
+		|| FALSE === preg_match('/[0-9]+/', $_POST['pass1'])
+		|| FALSE === preg_match('/[~`!@#\$%\^&\*\(\)_\+\{\}\|"<>\?\-=\[\];\',\.]+/', $_POST['pass1'])) {
+	
+}
+
+// create the user
+$random_key = random_bytes(SODIUM_CRYPTO_SECRETBOX_KEYBYTES);
+$iterations = 1000;
+
+$nonce = random_bytes(SODIUM_CRYPTO_SECRETBOX_NONCEBYTES);
+$derived_pwd_key = hash_pbkdf2('sha256', $_POST['pass1']. 'hjg', $nonce,
+		$iterations, SODIUM_CRYPTO_SECRETBOX_KEYBYTES);
+$pwd_key = $derived_pwd_key ^ $random_key;
+
+$derived_backup_key = hash_pbkdf2('sha256', $_POST['email']. 'oiuyut', $nonce,
+		$iterations, SODIUM_CRYPTO_SECRETBOX_KEYBYTES);
+$backup_key = $derived_backup_key ^ $random_key;
+
+echo '<br />' . $derived_pwd_key;
+echo '<br />' . $derived_backup_key;
+echo '<br />' . $pwd_key;
+echo '<br />' . $backup_key;
+echo '<br />' . ($derived_pwd_key ^ $pwd_key);
+echo '<br />' . ($derived_backup_key ^ $backup_key);
 
 // redirect user
 ?>

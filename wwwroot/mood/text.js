da8dbd7893d2dd9a4f8dc235aafdcc72c6e044bd
ImprobabilityCast <segmentationfault3@outlook.com>
2020-07-19 17:59:24 -0400

    display text done

--- wwwroot/mood/text.js
+++ wwwroot/mood/text.js
@@ -1,10 +1,8 @@
 var text = (function () {
     let obj = {};
 
-    function createText(stamp, text, name) {
-        let stampStr = stamp.replace(/[^0-9]/g, '_');
-        console.log(stampStr);
-        let id = name + stampStr;
+    function createText(stamp, html) {
+        let id = 'a' + (Math.random() + stamp).replace(/[^0-9]/g, '_');
 
         let link = document.createElement('a');
         link.innerText = stamp;
@@ -16,8 +14,9 @@ var text = (function () {
 
         let hidden = document.createElement("div");
         hidden.classList.add("collapse");
+        hidden.classList.add("pt-2")
         hidden.setAttribute("id", id);
-        hidden.innerText = text;
+        hidden.appendChild(html);
 
         let container = document.createElement("div");
         container.classList.add("text-card");
@@ -27,21 +26,101 @@ var text = (function () {
         return container;
     }
 
-    function insertText(parent, dataset, names) {
+    function insertText(parent, dataset) {
         parent.innerHTML = "";
         for (let entry of dataset) {
-            for (let name of names) {
-                console.log(entry[name]);
-                if (entry[name].length > 0) {
-                    let text = createText(entry.stamp, entry[name], name);
-                    parent.appendChild(text);
-                }
-            }
+            let html = createText(entry.stamp, entry.html);
+            parent.appendChild(html);
         }
     }
 
+    function transformName(name) {
+        let arr = {
+            "emote_response" : "Emotional Response",
+            "expression" : "Expressions",
+            "thought" : "Thoughts",
+            "what_do" : "What do",
+            "what_impact" : "Impact",
+        };
+
+        if (arr[name] !== undefined) {
+            return arr[name];
+        } else {
+            let words = name.split("_");
+            for (let i = 0; i < words.length; i++) {
+                words[i] = words[i].charAt(0).toUpperCase() + words[i].substr(1);
+            }
+            return words.join(" ");
+        }
+    }
+
+    function dataToHtml(entry) {
+        let div = document.createElement("div");
+        let strCount = 0;
+        for (let prop of Object.getOwnPropertyNames(entry)) {
+            if (typeof entry[prop] === "string" && prop !== "stamp") {
+                let head = document.createElement('h6');
+                head.innerText = transformName(prop);
+                head.classList.add("font-weight-bold");
+
+                strCount += 1;
+                if (strCount == 1) {
+                    head.style.display = "none";
+                }
+
+                let text = document.createElement('p');
+                text.innerText = entry[prop];
+                div.appendChild(head);
+                div.appendChild(text);
+            }
+        }
+        
+        if (strCount > 1) {
+            div.children[0].style.display = "block";
+        }
+        return div;
+    }
+    
+    function transformData(data) {
+        let trans = {};
+        let mechHolder = data.mechs;
+        delete data.mechs;
+
+        for (let group of Object.getOwnPropertyNames(data)) {
+            trans[group] = [];
+            if (group === 'sleep') {
+                for (let entry of data[group]) {
+                    trans[group].push({});
+                    let div = document.createElement('div');
+                    div.appendChild(document.createElement('p'));
+                    div.children[0].innerText = entry.meds;
+                    trans[group][trans[group].length - 1].stamp = entry.stamp;
+                    trans[group][trans[group].length - 1].html = div;
+                }
+            } else {
+                for (let entry of data[group]) {
+                    trans[group].push({});
+                    let div = dataToHtml(entry);
+                    trans[group][trans[group].length - 1].stamp = entry.stamp;
+                    trans[group][trans[group].length - 1].html = div;
+                }
+            }
+        }
+
+        data.mechs = mechHolder;
+        return trans;
+    }
+
     obj.addText = function (data) {
-        insertText($('#suicide-text')[0], data.suicide, ['steps']);
+        let trans = transformData(data);
+        console.log(trans);
+        insertText($('#suicide-text')[0], trans.suicide);
+        insertText($('#self-harm-text')[0], trans.self_harm);
+        insertText($('#anxiety-text')[0], trans.anxiety);
+        insertText($('#anger-text')[0], trans.anger);
+        insertText($('#sleep-text')[0], trans.sleep);
+        insertText($('#people-text')[0], trans.people);
+        insertText($('#notes-text')[0], trans.notes);
     }
 
     return obj;

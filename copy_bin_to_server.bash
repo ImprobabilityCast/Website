22d28e69c13d380ceff6ac4df9fd80887e9b96e6
ImprobabilityCast <1segfault@pm.me>
2025-12-05 00:32:40 -0500

    fix update scripts

--- copy_bin_to_server.bash
+++ copy_bin_to_server.bash
@@ -7,16 +7,23 @@ SCP_OPTIONS="-P 23 -i $2"
 SSH_OPTIONS="-p 23 -i $2"
 HOSTY=$1@adoodleyo.dev
 
+# git branch -D to_prod
+# git checkout -b to_prod
+# source ./cattle_prod.bash
+# git commit -m "to prod"
+# git checkout prod
+# git merge to_prod
+# git push
 
-# FILE_PATH=/config/prod.toml
-# scp $SCP_OPTIONS $REPO$FILE_PATH $HOSTY:$REPO$FILE_PATH
+FILE_PATH=/config/prod.toml
+scp $SCP_OPTIONS $REPO$FILE_PATH $HOSTY:$REPO$FILE_PATH
 # FILE_PATH=/config/mood_db_php_user_pwd.txt
 # scp $SCP_OPTIONS $REPO$FILE_PATH $HOSTY:$REPO$FILE_PATH
 FILE_PATH=/update_server.bash
-scp $SCP_OPTIONS $REPO$FILE_PATH $HOSTY:$REPO$FILE_PATH
+scp $SCP_OPTIONS $REPO$FILE_PATH $HOSTY:$REPO/..$FILE_PATH
 FILE_PATH=/main/clueless/pkg/
 ssh $SSH_OPTIONS $HOSTY "mkdir -p $REPO$FILE_PATH"
 scp $SCP_OPTIONS -r $REPO$FILE_PATH $HOSTY:$REPO$FILE_PATH
 
 
-ssh $SSH_OPTIONS $HOSTY sudo $REPO/update_server.bash
+ssh $SSH_OPTIONS -t $HOSTY sudo $REPO/../update_server.bash

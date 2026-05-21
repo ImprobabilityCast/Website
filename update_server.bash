635a2d7dcb72670f9e5455b0aa651a1be316457e
ImprobabilityCast <1segfault@pm.me>
2026-05-21 01:02:48 -0400

    update the updater

--- update_server.bash
+++ update_server.bash
@@ -5,18 +5,4 @@ cd $REPO
 
 git pull
 
-# update django if needed
-source $REPO/um/env/bin/activate
-cd $REPO/um
-python -m pip install -r $REPO/um/proj/requirements.txt
-python manage.py migrate
-python manage.py collectstatic --noinput
-deactivate
-
-# update config if needed
-cd $REPO/config
-cp ports.conf apache2.conf /etc/apache2/
-cp 999-main.conf 001-um.conf 000-default.conf /etc/apache2/sites-available/
-cd ../
-
-source ./restart.bash
+source ./update_server_additions.bash
